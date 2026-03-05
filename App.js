312b7e64ad69b6a4737ed028e3b69bd6f03aa9ac
Vivek Tiwari <vivekt123456@gmail.com>
2026-03-06 01:58:37 +0530

    EP3-half

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent =
-    React.createElement("div", { id: "parent" }, [
-        React.createElement("div", { id: "child" },
-            [React.createElement("h1", { id: "h1" }, "Hello from h178"), React.createElement("h2", { id: "h2" }, "Hello from h2")]),
-        React.createElement("div", { id: "child2" },
-            [React.createElement("h3", { id: "h4" }, "Hello from h3"), React.createElement("h4", { id: "h4" }, "Hello from h9")])]);
 
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading  = React.createElement("h1",{id:"heading"},"Namaste React");
+const root  = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+const jsxheading = (<h1 className="heading">Namaste Vivek</h1>);
+
+root.render(jsxheading);
